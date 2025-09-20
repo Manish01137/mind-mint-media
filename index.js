34ad5bd1a8995ff7b5d000252cd7812d0dee1407
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2025-09-20 11:34:26 +0530

    second

---
+++ index.js
@@ -0,0 +1,15 @@
+// JavaScript for toggle FAQ
+const faqButtons = document.querySelectorAll(".faq-question");
+
+faqButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+
+    // Toggle open
+    if(answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+});
